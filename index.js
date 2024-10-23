5ec6129da24a497d6eabda7b85bb7dc4e919393e
chimi <deadxraver@yandex.ru>
2024-10-23 13:29:20 +0300

    lab is done!

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ let x = -3;
 let y = null;
 let r = null;
 let buttob;
+let canvasDrawer;
 
 function checkNum(a) {
 	return !isNaN(a) && a !== null;
 }
 
-document.addEventListener("DOMContentLoaded",  () => {
+document.addEventListener("DOMContentLoaded", () => {
+	canvasDrawer = new CanvasDrawer();
+	canvasDrawer.redrawAll();
 	buttob = document.getElementById('buttob');
 	buttob.style.visibility = 'hidden';
 
@@ -16,7 +19,6 @@ document.addEventListener("DOMContentLoaded",  () => {
 	const rList = [];
 
 	for (let i = 1; i < 6; i++) {
-
 		rList.push(document.getElementById("r".concat(i.toString())));
 		let fun = () => {
 			for (let j = 0; j < rList.length; j++) {
@@ -25,6 +27,7 @@ document.addEventListener("DOMContentLoaded",  () => {
 				}
 			}
 			r = rList[i - 1].checked ? i : null;
+			canvasDrawer.redrawAll(r)
 			if (checkNum(x) && checkNum(parseFloat(y)) && checkNum(r)) {
 				buttob.style.visibility = 'visible';
 			} else buttob.style.visibility = 'hidden';
@@ -64,16 +67,53 @@ document.addEventListener("DOMContentLoaded",  () => {
 			buttob.style.visibility = 'visible';
 		} else buttob.style.visibility = 'hidden';
 	});
+
+	buttob.addEventListener("click", () => {
+		if (validate()) sendForm();
+		else alert("Проверьте корректность введенных значений");
+	});
+
+	function processError() {
+		alert("Бредик не пишем");
+	}
+
+	function sendForm() {
+		$.ajax({
+			type: "GET",
+			url: "http://localhost:24928/fcgi-bin/server.jar",
+			data: {x: x, y: y, r: r},
+			success: (msg) => {
+				processSuccess($.parseJSON(msg));
+			},
+			error: (msg) => {
+				processError();
+			}
+		});
+	}
 });
 
+function validate() {
+	return checkNum(x) && checkNum(y) && checkNum(r) && (x >= -3 && x <= 5) && (y >= -3 && y <= 5) && (r >= 1 && r <= 5);
+}
 
-function sendForm() {
-	$.ajax({
-		type: "GET",
-		url: "http://localhost:63342",
-		data: {x: x, y: y, r: r},
-		success: (msg) => {
-			alert( "Data Saved: " + msg );
-		}
-	});
+function processSuccess(msg) {
+	canvasDrawer.drawPoint(x, y, msg.hit === "true");
+	console.log(msg);
+
+	let row = document.getElementById("resultsBody").insertRow();
+
+	row.insertCell(0).textContent = msg.x;
+	console.log(msg.x);
+	row.insertCell(1).textContent = parseFloat(msg.y.replace(',', '.')).toFixed(2);
+	console.log(parseFloat(msg.y).toFixed(2));
+	console.log(parseFloat(msg.y));
+	row.insertCell(2).textContent = msg.r;
+	console.log(msg.r);
+	row.insertCell(3).textContent = msg.hit === "true" ? "Да" : "Нет";
+	console.log(msg.hit);
+	[row.insertCell(4).textContent] = msg.currentTime.replace('T', '\n').split('.');
+	console.log(msg.currentTime);
+	row.insertCell(5).textContent = msg.executionTime + 'ms';
+	console.log(msg.executionTime);
+
 }
